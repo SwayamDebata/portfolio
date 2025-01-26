85f1eae90d89d106fc76dbd99ef4e33c6e616330
SwayamDebata <swayamdebata2003@gmail.com>
2025-01-26 19:49:01 +0530

    updated resume

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const RESUME_URL = "https://drive.google.com/file/d/1EHmSMZeZaY6vCvAgFYEYPMmjPFaVBOVX/view?usp=sharing";
+export const RESUME_URL = "https://drive.google.com/file/d/1L-cs6bwXis97Ee8lxj98irr72iRnKov3/view?usp=sharing";
 export const HTML_DOC_URL = "https://www.w3.org/2010/Talks/1117-html5-plh/basic_html5_doc.html";
 export const CSS_DOC_URL = "https://developer.mozilla.org/en-US/docs/Web/CSS";
 export const JS_DOC_URL = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
